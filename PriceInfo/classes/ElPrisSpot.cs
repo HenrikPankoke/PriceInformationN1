bbc26e5c497873f0bcdf9bf6d5e3888441867e33
CP-SDBG <123507013+CP-SDBG@users.noreply.github.com>
2024-04-14 13:48:41 +0200

    added new price source

---
+++ PriceInfo/classes/ElPrisSpot.cs
@@ -0,0 +1,13 @@
+﻿namespace PriceInfo.Classes
+{
+    public class ElPrisSpot
+    { 
+        public double DKK_per_kWh { get; set; }
+        public double EUR_per_kWh { get; set; }
+        public double EXR { get; set; }
+        public DateTime time_start { get; set; }
+        public DateTime time_end { get; set; }
+    }
+
+
+}
